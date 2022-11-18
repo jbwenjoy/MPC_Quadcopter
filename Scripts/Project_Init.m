a1bede3f4fb7a68899d60f408dcbdf93cac87e80
jbwenjoy <jbwenjoy@outlook.com>
2022-11-18 22:37:30 +0800

    Update

--- Scripts/Project_Init.m
+++ Scripts/Project_Init.m
@@ -5,10 +5,10 @@ load("MPCDesignerSession.mat");
 
 open("Models\MPC_Quad.slx")
 
-% g = 9.81;
-% mass = 4;
-% Ixx = 3.5e-3;
-% Iyy = 3.5e-3;
-% Izz = 7e-3;
-% Ir = 1.5e-5;
-% length = 0.3;
+g = 9.81;
+mass = 4;
+Ixx = 3.5e-3;
+Iyy = 3.5e-3;
+Izz = 7e-3;
+Ir = 1.5e-5;
+length = 0.3;
