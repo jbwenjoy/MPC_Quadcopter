d7ce5909023eb4498cc61eb53ea676c3f49fb55f
jbwenjoy <jbwenjoy@outlook.com>
2023-02-21 10:17:28 +0800

    Remove trash

--- Scripts/mpc1_design.m
+++ Scripts/mpc1_design.m
@@ -8,32 +8,32 @@ mpc1.PredictionHorizon = 30;
 
 %% specify control horizon
 disp('ControlHorizon = 40');
-mpc1.ControlHorizon = 29;
+mpc1.ControlHorizon = 2;
 
 %% specify nominal values for inputs and outputs
 disp('Nominal.U = [4400; 4400; 4400; 4400]');
-mpc1.Model.Nominal.U = [4400; 4400; 4400; 4400];
+mpc1.Model.Nominal.U = [4355; 4355; 4355; 4355];
 % mpc1.Model.Nominal.U = [5000; 5000; 5000; 5000];
 disp('Nominal.Y = [0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0]');
 mpc1.Model.Nominal.Y = [0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0];
 
 %% specify scale factors for inputs and outputs
-mpc1.MV(1).ScaleFactor = 10000;
-mpc1.MV(2).ScaleFactor = 10000;
-mpc1.MV(3).ScaleFactor = 10000;
-mpc1.MV(4).ScaleFactor = 10000;
-mpc1.OV(1).ScaleFactor = 100;
-mpc1.OV(2).ScaleFactor = 10;
-mpc1.OV(3).ScaleFactor = 100;
-mpc1.OV(4).ScaleFactor = 10;
-mpc1.OV(5).ScaleFactor = 100;
-mpc1.OV(6).ScaleFactor = 10;
-mpc1.OV(7).ScaleFactor = 1;
-mpc1.OV(8).ScaleFactor = 1;
-mpc1.OV(9).ScaleFactor = 1;
-mpc1.OV(10).ScaleFactor = 1;
-mpc1.OV(11).ScaleFactor = 3.1416;
-mpc1.OV(12).ScaleFactor = 3.1416;
+mpc1.MV(1).ScaleFactor = 10000; % n1
+mpc1.MV(2).ScaleFactor = 10000; % n2
+mpc1.MV(3).ScaleFactor = 10000; % n3
+mpc1.MV(4).ScaleFactor = 10000; % n4
+mpc1.OV(1).ScaleFactor = 100; % x
+mpc1.OV(2).ScaleFactor = 10; % vx
+mpc1.OV(3).ScaleFactor = 100; % y
+mpc1.OV(4).ScaleFactor = 10; % vy
+mpc1.OV(5).ScaleFactor = 10; % z
+mpc1.OV(6).ScaleFactor = 1; % vz
+mpc1.OV(7).ScaleFactor = 1; % phi
+mpc1.OV(8).ScaleFactor = 1; % p
+mpc1.OV(9).ScaleFactor = 1; % theta
+mpc1.OV(10).ScaleFactor = 1; % q
+mpc1.OV(11).ScaleFactor = 2 * 3.1416; % psi
+mpc1.OV(12).ScaleFactor = 2 * 3.1416; % r
 
 %% specify constraints for MV and MV Rate
 mpc1.MV(1).Min = 0;
@@ -91,7 +91,7 @@ mpc1.OV(12).Max = 3.14159265359;
 %% specify weights
 mpc1.Weights.MV = [1 1 1 1];
 mpc1.Weights.MVRate = [1 1 1 1];
-mpc1.Weights.OV = [10 1 10 1 100 10 1 1 1 1 0.1 1];
+mpc1.Weights.OV = [10 1 10 1 100 1 1 1 1 1 1 10];
 mpc1.Weights.ECR = 100000;
 
 %% specify simulation options
